d6884735b5e1a128cf5a6853ef3f10ee43f2be7d
BilelJm <bileljemai97@gmail.com>
2023-08-25 10:49:41 +0100

    UserComponent Updated !

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SecurityService } from '../service/security-service';
-import { TestService } from '../service/Test-servie';
+import { KeycloakService } from 'keycloak-angular'; // Import KeycloakService
 
 @Component({
   selector: 'app-navbar',
@@ -9,24 +9,24 @@ import { TestService } from '../service/Test-servie';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(public securityService: SecurityService, private testservice : TestService) { }
+  constructor(public securityService: SecurityService, private keycloakService: KeycloakService) { }
 
   public async ngOnInit() {
-
+    // No specific initialization required
   }
 
   onLogout() {
-    this.securityService.kcService.logout(window.location.origin);
+    this.keycloakService.logout(window.location.origin); // Use keycloakService here
   }
  
-  public async getToken() {
-  }
+  async loginAndSendToken(){
+    await this.keycloakService.login({
+      redirectUri: window.location.origin
+    });
 
-  async login(){
-    await this.securityService.kcService.login({
-      redirectUri : window.location.origin
-    })
-    console.log("login");
+    // Get token from keycloakService
+    const token = this.keycloakService.getKeycloakInstance().token;
+    window.parent.postMessage({ type: 'auth_token', token }, 'http://localhost:4300');
+    
   }
-
 }
